f263aac7901be8b7912e9556ccc36fe6ddd6ade5
hotheadhacker <isalmanqureshi@gmail.com>
2020-05-02 12:48:17 +0530

    Added Mongoose modals for Users Route

---
+++ Routes/modals/User.js
@@ -0,0 +1,26 @@
+const mongooese = require('mongoose');
+
+const UserSchema = new mongooese.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model('user', User);
