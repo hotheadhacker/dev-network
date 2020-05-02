61e9b8381397179b1cd4040267e527e0c3b36c1e
hotheadhacker <isalmanqureshi@gmail.com>
2020-05-02 14:05:10 +0530

    :tada: added validations on user.js

--- Routes/api/users.js
+++ Routes/api/users.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-//@rout     GET api/users
-//@desc     Test Rout
+//@rout     POST api/users
+//@desc     Register User
 //@access   Public
-router.get('/', (req, res) => res.send('User Roure'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more chars').isLength({
+      min: 6
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User Roure');
+  }
+);
 
 module.exports = router;
