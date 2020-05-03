c021b00715d95b12c6552ff77102cce7f7cf8cd2
hotheadhacker <isalmanqureshi@gmail.com>
2020-05-03 17:08:30 +0530

    :bulb: added experience

--- Routes/api/profile.js
+++ Routes/api/profile.js
@@ -142,4 +142,85 @@ router.get('/users/:user_id', async (req, res) => {
     res.status(500).send('Server Error!');
   }
 });
+
+//@route     DELETE api/Profile
+//@desc      Delete profile,user & post
+//@access    Private
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    //   @todo - remove users posts
+
+    // Remove Profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove User
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: 'User Deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error!');
+  }
+});
+
+//@route     PUT api/profile/experience
+//@desc      Add profile experience
+//@access    Private
+
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required')
+        .not()
+        .isEmpty(),
+      check('company', 'Company is required')
+        .not()
+        .isEmpty(),
+      check('from', 'From date is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array() });
+    }
+
+    //destructuring from body
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newExp = {
+      // title: tiltle
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.experience.unshift(newExp);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error!');
+    }
+  }
+);
+
 module.exports = router;
