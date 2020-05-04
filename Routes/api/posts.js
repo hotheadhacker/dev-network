fbd7dd704fca0fd146a7b4d246f624a5b7df7c02
hotheadhacker <isalmanqureshi@gmail.com>
2020-05-04 07:15:40 +0530

    :books: Added Get all post, Get PostById & Delete Post

--- Routes/api/posts.js
+++ Routes/api/posts.js
@@ -7,7 +7,7 @@ const Post = require('../../modals/Post');
 const User = require('../../modals/User');
 const Profile = require('../../modals/Profile');
 
-//@rout     POST api/Post
+//@rout     POST api/posts
 //@desc     Create a post
 //@access   Private
 router.post(
@@ -43,5 +43,64 @@ router.post(
     }
   }
 );
+
+//@rout     GET api/Posts
+//@desc     Get all posts
+//@access   Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error!');
+  }
+});
+
+//@rout     GET api/posts/:id
+//@desc     Get post by ID
+//@access   Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: 'Post Not found' });
+    }
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.name == 'CastError') {
+      return res.status(404).json({ msg: 'Post Not found' });
+    }
+    res.status(500).send('Server Error!');
+  }
+});
+
+//@rout     DELETE api/posts/:id
+//@desc     Delete a posts
+//@access   Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: 'Post Not found' });
+    }
+
+    //User that deletes the post owns the post
+    //Check user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User Not Authorized!' });
+    }
+    await post.remove();
+
+    res.json({ msg: 'Post Removed' });
+  } catch (err) {
+    console.error(err.message);
+    if (err.name == 'CastError') {
+      return res.status(404).json({ msg: 'Post Not found' });
+    }
+    res.status(500).send('Server Error!');
+  }
+});
 
 module.exports = router;
